2a6f57c543d60e40669d87c1c6bad84265f2fad1
Doug Gerber <dojoh@outlook.com>
2013-03-19 21:31:30 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phinsaqu::Application.config.secret_token = '7139023fc3b6f3ba94cb14b4be21e45ff4640d4f14d9a7f7ed23bc5c6479e77157d258362b6f5611064be7839eaa3aa5a9dd28eb9303d05ea40709d8a19011c2'
